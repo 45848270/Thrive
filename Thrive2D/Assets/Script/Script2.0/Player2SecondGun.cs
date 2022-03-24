0c841c750c18ca4bee3394ec1993af76afc8a5df
45848270 <shubhamchandra.yadav@students.mq.edu.au>
2022-03-24 22:40:40 +1100

    Organized version of assets

--- Thrive2D/Assets/Script/Script2.0/Player2SecondGun.cs
+++ Thrive2D/Assets/Script/Script2.0/Player2SecondGun.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1SecondGun : MonoBehaviour
+public class Player2SecondGun : MonoBehaviour
 {
     public Transform initialPos;
     public GameObject bulletPrefab;
@@ -12,7 +12,7 @@ public class Player1SecondGun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    if (Input.GetKeyDown(KeyCode.Space)) 
+    if (Input.GetKeyDown(KeyCode.Keypad5)) 
         {
             shoot();
         }

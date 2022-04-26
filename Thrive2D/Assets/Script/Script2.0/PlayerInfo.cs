a6fb87799e0c0d1bdb2968eba1dbfa3f8ec13961
arjun-kalia <arjun.kalia@students.mq.edu.au>
2022-04-26 21:26:15 +1000

    Added PlayerInfo UI

--- Thrive2D/Assets/Script/Script2.0/PlayerInfo.cs
+++ Thrive2D/Assets/Script/Script2.0/PlayerInfo.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInfo : MonoBehaviour{
-
-public Gameobject Panel;
-
-public void OpenPanel()
+public class PlayerInfo : MonoBehaviour
 {
-    if(Panel != null)
+
+    public GameObject Panel1;
+    public GameObject Panel2;
+
+
+
+    void Update()
     {
-        Panel.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Show Panel");
+            bool isActive = Panel1.activeSelf;
+            Panel1.SetActive(!isActive);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            bool isActive = Panel2.activeSelf;
+            Panel2.SetActive(!isActive);
+        }
+
     }
-}
 
 
 }

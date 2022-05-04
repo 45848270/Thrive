626455a43d21584dafe9a2dcbaf2975b8a12349e
arjun-kalia <arjun.kalia@students.mq.edu.au>
2022-05-04 21:27:44 +1000

    Added cannon

---
+++ Thrive2D/Assets/Script/Script2.0/Cannon1.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon1 : MonoBehaviour
+{
+    public static Cannon1 instance;
+    public Transform initialPos;
+    public GameObject cannonShotPrefab;
+    public float bulletForce = 20f;
+    public float reloadTime = 2f;
+
+    public float timeKeeper;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        timeKeeper = reloadTime;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeKeeper <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                shoot();
+                timeKeeper = reloadTime;
+            }
+        }
+        else
+        {
+            timeKeeper -= Time.deltaTime;
+
+        }
+
+    }
+
+    void shoot()
+    {
+        //shooting method
+        GameObject cannon = Instantiate(cannonShotPrefab, initialPos.position, initialPos.rotation);
+        // bullet.GetComponent <Player1bullet >().
+    }
+
+    // public void Decrease_P1_ReloadTime()
+    // {
+    //     reloadTime -= (0.15f * reloadTime);
+    // }
+}

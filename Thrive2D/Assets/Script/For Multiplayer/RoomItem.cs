8439ac33337471fedc0d65789452ffb8f77b4b6d
arjun-kalia <arjun.kalia@students.mq.edu.au>
2022-05-14 22:50:41 +1000

    testing buttons

--- Thrive2D/Assets/Script/For Multiplayer/RoomItem.cs
+++ Thrive2D/Assets/Script/For Multiplayer/RoomItem.cs
@@ -7,22 +7,23 @@ using TMPro;
 
 public class RoomItem : MonoBehaviour
 {
-   public TMP_Text roomName;
-   CreateAndJoinRooms manger;
+    public TMP_Text roomName;
+    CreateAndJoinRooms manger;
 
-   private void Start()
-   {
-       manger=FindObjectOfType<CreateAndJoinRooms>();
-   }
+    private void Start()
+    {
+        manger = FindObjectOfType<CreateAndJoinRooms>();
+    }
 
-   public void SetRoomName(string _roomName)
-   {
-       roomName.text=_roomName;
-   }
+    public void SetRoomName(string _roomName)
+    {
+        roomName.text = _roomName;
+    }
 
-   public void OnClickItem()
-   {
-       manger.JoinRoom(roomName.text);
-   }
+    public void OnClickItem()
+    {
+        manger.JoinRoom(roomName.text);
+        Debug.Log("working");
+    }
 
 }

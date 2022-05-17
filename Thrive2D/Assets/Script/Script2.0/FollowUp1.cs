e9f9750563fe3518bb49e687ca744d1b28d5478c
45848270 <shubhamchandra.yadav@students.mq.edu.au>
2022-05-17 19:14:24 +1000

    changes to v1

--- Thrive2D/Assets/Script/Script2.0/FollowUp1.cs
+++ Thrive2D/Assets/Script/Script2.0/FollowUp1.cs
@@ -4,27 +4,34 @@ using UnityEngine;
 
 public class FollowUp1 : MonoBehaviour
 {
-    public Transform player;
+    public GameObject player;
     public float moveSpeed=20;
-    //public ParticleSystem ps;
+    public ParticleSystem ps;
     private Rigidbody2D rb;
     private Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb=this.GetComponent<Rigidbody2D>();    
+        rb=this.GetComponent<Rigidbody2D>();  
+          if(player == null)
+        {
+            player=GameObject.FindWithTag("Player1");
+        }  
+        ps.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction =player.position-transform.position;
+        Vector3 direction =player.transform.position-transform.position;
         float angle =Mathf.Atan2(direction.y,direction.x)*Mathf.Rad2Deg;
 
         rb.rotation=angle;
         direction.Normalize();
         movement=direction;
+
+         Destroy(gameObject,3f);
     }
     void FixedUpdate() 
     {
@@ -34,5 +41,13 @@ public class FollowUp1 : MonoBehaviour
     void moveCharacter(Vector2 direction)
     {
         rb.MovePosition((Vector2)transform.position +(direction*moveSpeed*Time.deltaTime));
+    }
+    void OnTriggerEnter2D(Collider2D collide)
+    {
+        if (collide.gameObject.tag.Equals("Player1"))
+        {
+            ps.Stop();
+           Destroy(gameObject); 
+        }
     }
 }
--- Thrive2D/Assets/Script/Script2.0/Followup2.cs
+++ Thrive2D/Assets/Script/Script2.0/Followup2.cs
@@ -4,27 +4,34 @@ using UnityEngine;
 
 public class Followup2 : MonoBehaviour
 {
-   public Transform player;
+   public GameObject player;
     public float moveSpeed=20;
-    //public ParticleSystem ps;
+    public ParticleSystem ps;
     private Rigidbody2D rb;
     private Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb=this.GetComponent<Rigidbody2D>();    
+        rb=this.GetComponent<Rigidbody2D>();  
+        if(player == null)
+        {
+            player=GameObject.FindWithTag("Player2");
+        }  
+        ps.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction =player.position-transform.position;
+        Vector3 direction =player.transform.position-transform.position;
         float angle =Mathf.Atan2(direction.y,direction.x)*Mathf.Rad2Deg;
 
         rb.rotation=angle;
         direction.Normalize();
         movement=direction;
+
+        Destroy(gameObject,3f);
     }
     void FixedUpdate() 
     {
@@ -34,5 +41,14 @@ public class Followup2 : MonoBehaviour
     void moveCharacter(Vector2 direction)
     {
         rb.MovePosition((Vector2)transform.position +(direction*moveSpeed*Time.deltaTime));
+    }
+
+     void OnTriggerEnter2D(Collider2D collide)
+    {
+        if (collide.gameObject.tag.Equals("Player2"))
+        {
+            ps.Stop();
+           Destroy(this.gameObject); 
+        }
     }
 }

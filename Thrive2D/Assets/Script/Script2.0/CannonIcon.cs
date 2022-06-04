1e6abf8bfd91218268d8fc980f9058d8a5657c20
Xiaosong Gao <xiaosong.gao@students.mq.edu.au>
2022-06-04 23:40:36 +1000

    fix buff ui num

--- Thrive2D/Assets/Script/Script2.0/CannonIcon.cs
+++ Thrive2D/Assets/Script/Script2.0/CannonIcon.cs
@@ -7,27 +7,32 @@ public class CannonIcon : Item
     public GameObject psRed;
     public GameObject psBlue;
 
+    private int cannonIconHp = 1;
+
 
     void OnTriggerEnter2D(Collider2D Cannon)
     {
-
-        if (Cannon.gameObject.tag.Equals("player1Bullet"))                                            //condition if it comes in contact with enemy
+        if(cannonIconHp>0)
         {
-            Destroy(Cannon.gameObject);
-            Instantiate(psRed,transform.position,Quaternion.identity);
-            Destroy(gameObject);
-            Activate_and_Deactivate.instance.Activate_Player1_Cannon();
-            Cannon1.instance.CannonShootOnce = true;
-        }
-        if (Cannon.gameObject.tag.Equals("player2Bullet"))                                            //condition if it comes in contact with enemy
-        {
-            Destroy(Cannon.gameObject);
-            Instantiate(psBlue,transform.position,Quaternion.identity);
-            Destroy(gameObject);
-            Activate_and_Deactivate.instance.Activate_Player2_Cannon();
-            Cannon2.instance.CannonShootOnce = true;
+            cannonIconHp-=2;
+            if (Cannon.gameObject.tag.Equals("player1Bullet"))                                            //condition if it comes in contact with enemy
+            {
+                Destroy(Cannon.gameObject);
+                Instantiate(psRed,transform.position,Quaternion.identity);
+                Destroy(gameObject);
+                Activate_and_Deactivate.instance.Activate_Player1_Cannon();
+                Cannon1.instance.CannonShootOnce = true;
+            }
+            if (Cannon.gameObject.tag.Equals("player2Bullet"))                                            //condition if it comes in contact with enemy
+            {
+                Destroy(Cannon.gameObject);
+                Instantiate(psBlue,transform.position,Quaternion.identity);
+                Destroy(gameObject);
+                Activate_and_Deactivate.instance.Activate_Player2_Cannon();
+                Cannon2.instance.CannonShootOnce = true;
 
  
+        }
         }
     }
 }
--- Thrive2D/Assets/Script/Script2.0/ReloadIcon.cs
+++ Thrive2D/Assets/Script/Script2.0/ReloadIcon.cs
@@ -7,27 +7,32 @@ public class ReloadIcon : Item
 
     public GameObject psRed;
     public GameObject psBlue;
+    private int reloadIconHP=1;
 
     void OnTriggerEnter2D(Collider2D ReloadIncrease)
     {
-
-        if (ReloadIncrease.gameObject.tag.Equals("player1Bullet"))                                            //condition if it comes in contact with enemy
+        if(reloadIconHP>0)
         {
-            ReloadA.incrementReloadA += 1;
-            Destroy(ReloadIncrease.gameObject);
-            Instantiate(psRed, transform.position, Quaternion.identity);
-            Destroy(gameObject);
-            Player1FirstGun.instance.Decrease_P1_ReloadTime();
-            Player1SecondGun.instance.Decrease_P1_ReloadTime();
+            reloadIconHP-=2;
+            if (ReloadIncrease.gameObject.tag.Equals("player1Bullet"))                                            //condition if it comes in contact with enemy
+            {
+                ReloadA.incrementReloadA += 1;
+                Destroy(ReloadIncrease.gameObject);
+                Instantiate(psRed, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+                Player1FirstGun.instance.Decrease_P1_ReloadTime();
+                Player1SecondGun.instance.Decrease_P1_ReloadTime();
+            }
+            if (ReloadIncrease.gameObject.tag.Equals("player2Bullet"))                                            //condition if it comes in contact with enemy
+            {
+                ReloadB.incrementReloadB += 1;
+                Destroy(ReloadIncrease.gameObject);
+                Instantiate(psBlue, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+                Player2FirstGun.instance.Decrease_P2_ReloadTime();
+                Player2SecondGun.instance.Decrease_P2_ReloadTime();
         }
-        if (ReloadIncrease.gameObject.tag.Equals("player2Bullet"))                                            //condition if it comes in contact with enemy
-        {
-            ReloadB.incrementReloadB += 1;
-            Destroy(ReloadIncrease.gameObject);
-            Instantiate(psBlue, transform.position, Quaternion.identity);
-            Destroy(gameObject);
-            Player2FirstGun.instance.Decrease_P2_ReloadTime();
-            Player2SecondGun.instance.Decrease_P2_ReloadTime();
         }
+        
     }
 }
